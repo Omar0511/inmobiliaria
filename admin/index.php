89dd11a4042f5b7f9a1fea918c88f23a9020b020
Omar Garcia <za19012225@zapopan.tecmm.edu.mx>
2024-03-09 11:28:25 -0600

    CReando una tabla para listar las propiedades de la bd

--- admin/index.php
+++ admin/index.php
@@ -19,6 +19,33 @@
         ?>
 
         <a href="/admin/propiedades/crear.php" class="boton boton-verde-inline-block">Nueva Propiedad</a>
+
+        <table class="propiedades">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Título</th>
+                    <th>Precio</th>
+                    <th>Imagen</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Casa en la Playa</td>
+                    <td>12999</td>
+                    <td>
+                        <img class="imagen-tabla" src="../imagenes/264210f3c8f9f2a901da1077f813c3dd.jpg" alt="">
+                    </td>
+                    <td>
+                        <a class="boton-rojo-block" href="">Eliminar</a>
+                        <a class="boton-amarillo-block" href="">Actualizar</a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </main>
 
 <?php
