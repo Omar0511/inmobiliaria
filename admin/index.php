84ed73ef5478ed746a07fa5f99a00867db00e4a4
Omar Garcia <za19012225@zapopan.tecmm.edu.mx>
2024-04-06 13:23:31 -0600

    Listar los VENDEDORES de la BD

--- admin/index.php
+++ admin/index.php
@@ -50,6 +50,7 @@
 
         <a href="/admin/propiedades/crear.php" class="boton boton-verde-inline-block">Nueva Propiedad</a>
 
+        <h2>Propiedades</h2>
         <table class="propiedades">
             <thead>
                 <tr>
@@ -85,11 +86,41 @@
                 <?php endforeach; ?>
             </tbody>
         </table>
+
+        <h2>Vendedores</h2>
+        <table class="propiedades">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nombre</th>
+                    <th>Teléfono</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+
+            <!-- Mostrar los resultados de la BD -->
+            <tbody>
+                <!-- foreach: es usado especialmente para recorrer arreglos -->
+                <?php foreach ($vendedores as $vendedor) : ?>
+                    <tr>
+                        <td> <?php echo $vendedor->id; ?> </td>
+                        <td> <?php echo $vendedor->nombre . " " . $vendedor->apellido; ?> </td>
+                        <td> <?php echo $vendedor->telefono; ?> </td>
+                        <td>
+                            <form method="POST" class="w-100">
+                                <input type="hidden" name="id" id="id" value="<?php echo $vendedor->id; ?>">
+
+                                <input type="submit" class="boton-rojo-block" value="Eliminar">
+                            </form>
+
+                            <a class="boton-amarillo-block" href="/admin/vemdedores/actualizar.php?id=<?php echo $vendedor->id;?>">Actualizar</a>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
     </main>
 
 <?php
-    // Cerrar conexión a la BD
-    mysqli_close($db);
-
     incluirTemplates('footer');
 ?>
